97f4a221e61c57ec45fdac8d4a401df03b4b4562
Nick Papacostas <nickpapacostas@learnzillion.com>
2014-04-21 20:24:02 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MtPleasantMixtape::Application.config.secret_key_base = 'ecc1b5cffcbce39ac82a3d8bc3740d76d109c1f8095e0adf83b0e0dd2d3241406bd08a963f690266b478321541d6f2cc24dfc83466935257d7e19f48adbeb2fd'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MtPleasantMixtape::Application.config.session_store :cookie_store, key: '_mt_pleasant_mixtape_session'
