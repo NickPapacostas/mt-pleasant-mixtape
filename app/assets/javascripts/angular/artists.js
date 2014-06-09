16a163f75913a5ea719cd93ee6cd2744e9ef50b2
Nick Papacostas <nickpapacostas@learnzillion.com>
2014-06-09 04:38:30 -0400

    artist and homepage working. need to add sound cloud and move to two controllers

--- app/assets/javascripts/angular/artists.js
+++ app/assets/javascripts/angular/artists.js
@@ -5,43 +5,39 @@ ArtistsApp = angular.module("ArtistsApp", ["ngResource", "ngSanitize", "ngRoute"
 ArtistsApp.config(['$routeProvider',
   function($routeProvider) {
     $routeProvider.
-      when('/artists/:id', {
-        templateUrl: 'artist.html'
-      }).
-      when('/homepage', {
-        templateUrl: 'homepage.html'
-      }).
-      otherwise({
-        redirectTo: '/homepage'
-      });
+    when('/artists/:id', {
+      templateUrl: 'artist.html',
+      controller: 'ArtistsCtrl'
+    }).
+    when('/homepage', {
+      templateUrl: 'homepage.html',
+      controller: 'ArtistsCtrl'
+    }).
+    otherwise({
+      redirectTo: '/homepage'
+    });
   }]);
 
-ArtistsApp.controller("ArtistsCtrl", ['$scope', "$sce", "$location", "ArtistFactory", function($scope, $sce, $location, ArtistFactory) {
+ArtistsApp.controller("ArtistsCtrl", ['$scope', "$sce", "$location", "$routeParams", "ArtistFactory", function($scope, $sce, $location, $routeParams, ArtistFactory) {
   $scope.initializeActiveArtist = function() {
-    angular.forEach($scope.artists, function(artist) {
-      if(artist.id == parseInt($location.path().replace("/",""))){
-        artist.active = true;
+    if($location.path().indexOf('artists') != -1){
+      if($scope.artists){
+        angular.forEach($scope.artists, function(artist) {
+          if(artist.id == parseInt($routeParams.id)){
+            artist.active = true;
+            $scope.activeArtist = artist
+          }
+        })
+
       }
-    });
+    }
   };
 
-  $scope.artistPath = function(artist){
-    $location.path(artist.id)
-  };
-
-  $scope.setPath = function(hash){
-    $scope.currentPage = hash;
-    $location.path(hash);
-  };
-
-  $scope.activeArtist = function(){
-    return $scope.artists.filter(function(a){ return a.active == true })[0]
-  };
-
-  $scope.artists = ArtistFactory.query();
-  $scope.artists.$promise.then( function(){
+  $scope.artistsPromise = ArtistFactory.query();
+  $scope.artistsPromise.$promise.then( function(data){
+    $scope.artists = data;
     $scope.initializeActiveArtist();
-  })
+  });
 
 } ])
 
