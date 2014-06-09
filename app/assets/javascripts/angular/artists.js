1568c44f65c654ccbf088c90c33ef372fc583615
Nick Papacostas <nickpapacostas@learnzillion.com>
2014-06-09 15:18:43 -0400

    added next artist support

--- app/assets/javascripts/angular/artists.js
+++ app/assets/javascripts/angular/artists.js
@@ -30,24 +30,42 @@ ArtistsApp.controller("ArtistsCtrl",
 ]);
 
 ArtistsApp.controller("ShowCtrl",
-  ['$scope', '$location', '$routeParams',
-  function($scope, $location, $routeParams) {
-    $scope.initializeActiveArtist = function(parent_artists) {
+  ['$scope', '$location', '$routeParams', '$anchorScroll',
+  function($scope, $location, $routeParams, $anchorScroll) {
+    // scroll top
+    $anchorScroll();
+
+    var sortArtists = function(artists) {
+      return artists.sort(function(a, b){
+        return parseInt(a.id) - parseInt(b.id)
+      });
+    };
+
+    $scope.initializeNextArtist = function(){
+      var sortedArtists = sortArtists($scope.parent_artists);
+      console.log(sortedArtists)
+      var activeArtistIndex = sortedArtists.indexOf($scope.activeArtist);
+      console.log(activeArtistIndex)
+      $scope.nextArtist = sortedArtists[activeArtistIndex + 1]
+    }
+
+    $scope.initializeActiveArtist = function() {
       if($location.path().indexOf('artists') != -1){
-        if(parent_artists){
-          angular.forEach(parent_artists, function(artist) {
+        if($scope.parent_artists){
+          angular.forEach($scope.parent_artists, function(artist) {
             if(artist.id == parseInt($routeParams.id)){
               artist.active = true;
               $scope.activeArtist = artist
             }
           });
+          $scope.initializeNextArtist();
         }
       }
     }
 
     $scope.$parent.artistsPromise.$promise.then( function(data){
       $scope.parent_artists = data;
-      $scope.initializeActiveArtist($scope.parent_artists);
+      $scope.initializeActiveArtist();
     });
   }
 ]);
