293ca7839fa3155f543c98c8e2984838556d7955
Nick Papacostas <nickpapacostas@learnzillion.com>
2014-06-09 04:55:41 -0400

    split into two controllers. still having the parallax on show page issue

--- app/assets/javascripts/angular/artists.js
+++ app/assets/javascripts/angular/artists.js
@@ -7,7 +7,7 @@ ArtistsApp.config(['$routeProvider',
     $routeProvider.
     when('/artists/:id', {
       templateUrl: 'artist.html',
-      controller: 'ArtistsCtrl'
+      controller: 'ShowCtrl'
     }).
     when('/homepage', {
       templateUrl: 'homepage.html',
@@ -18,28 +18,39 @@ ArtistsApp.config(['$routeProvider',
     });
   }]);
 
-ArtistsApp.controller("ArtistsCtrl", ['$scope', "$sce", "$location", "$routeParams", "ArtistFactory", function($scope, $sce, $location, $routeParams, ArtistFactory) {
-  $scope.initializeActiveArtist = function() {
-    if($location.path().indexOf('artists') != -1){
-      if($scope.artists){
-        angular.forEach($scope.artists, function(artist) {
-          if(artist.id == parseInt($routeParams.id)){
-            artist.active = true;
-            $scope.activeArtist = artist
-          }
-        })
 
+ArtistsApp.controller("ArtistsCtrl",
+  ['$scope', "$sce", "$location", "$routeParams", "ArtistFactory",
+  function($scope, $sce, $location, $routeParams, ArtistFactory) {
+    $scope.artistsPromise = ArtistFactory.query();
+    $scope.artistsPromise.$promise.then( function(data){
+      $scope.artists = data;
+    });
+  }
+]);
+
+ArtistsApp.controller("ShowCtrl",
+  ['$scope', '$location', '$routeParams',
+  function($scope, $location, $routeParams) {
+    $scope.initializeActiveArtist = function(parent_artists) {
+      if($location.path().indexOf('artists') != -1){
+        if(parent_artists){
+          angular.forEach(parent_artists, function(artist) {
+            if(artist.id == parseInt($routeParams.id)){
+              artist.active = true;
+              $scope.activeArtist = artist
+            }
+          });
+        }
       }
     }
-  };
 
-  $scope.artistsPromise = ArtistFactory.query();
-  $scope.artistsPromise.$promise.then( function(data){
-    $scope.artists = data;
-    $scope.initializeActiveArtist();
-  });
-
-} ])
+    $scope.$parent.artistsPromise.$promise.then( function(data){
+      $scope.parent_artists = data;
+      $scope.initializeActiveArtist($scope.parent_artists);
+    });
+  }
+]);
 
 ArtistsApp.factory("ArtistFactory", ['$resource', function($resource) {
   return $resource("/artists.json");
