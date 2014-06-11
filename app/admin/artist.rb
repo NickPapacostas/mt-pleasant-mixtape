fbaa4b860f330356018527c4b7b60b2c368961ab
Nick Papacostas <nickpapacostas@learnzillion.com>
2014-06-11 17:47:05 -0400

    background image from s3 shown if provided, otherwise use example one (for now)

--- app/admin/artist.rb
+++ app/admin/artist.rb
@@ -1,11 +1,12 @@
 ActiveAdmin.register Artist do
-  permit_params :name, :song_name, :song_url, :description, :story_of_the_band, :story_of_the_song, :favorite_dc_thing
+  permit_params :name, :background_image_url, :song_name, :song_url, :description, :story_of_the_band, :story_of_the_song, :favorite_dc_thing
 
   form do |f|
     f.actions
 
     f.inputs do
       f.input :name
+      f.input :background_image_url
       f.input :song_name
       f.input :song_url
       f.input :description, as: :wysihtml5, commands: :all, blocks: :all
